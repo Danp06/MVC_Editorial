f43e09e27c72122107f5462dbee5f687af46d396
Danp06 <99297786+Danp06@users.noreply.github.com>
2022-02-17 17:09:32 -0500

    Update document.py

--- logic/document.py
+++ logic/document.py
@@ -7,36 +7,30 @@ class Document(object):
                  ,edition: int = 'edicion', nropag: int = 'numeropaginas'):
         """ Person constructor object.
 
-        :param id_person: id of person.
-        :type id_person: int
-        :param name: name of person.
-        :type name: str
-        :param last_name: last name of person.
-        :type last_name: str
-        :returns: Person object
-        :rtype: object
+        :param tittle: name of document.
+        :type tittle: str
         """
         self._tittle = tittle
 
     @property
     def tittle(self) -> str:
-        """ Returns the name of the person.
-          :returns: name of person.
+        """ Returns the name of the document.
+          :returns: name of document.
           :rtype: str
         """
         return self._tittle
 
     @tittle.setter
     def tittle(self, tittle: str):
-        """ The name of the person.
-        :param name: name of person.
+        """ The name of the document.
+        :param name: name of document.
         :type: str
         """
         self._tittle = tittle
 
     def __str__(self):
-        """ Returns str of person.
-          :returns: sting person
+        """ Returns str of document.
+          :returns: sting document
           :rtype: str
         """
         return '({0})'.format( self.tittle)
@@ -44,7 +38,7 @@ class Document(object):
 
 if __name__ == '__main__':
 
-    edwin = Document(tittle="Edwin")
-    edwin.name = "Edwin. A"
-    print(edwin)
+    edwin = Document(tittle="El principito")
+    edwin.name = "Principito"
+    print(Document)
 
